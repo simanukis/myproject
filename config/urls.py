4d475066e992598af93f659367b9120d1bc2469a
sunlit-ind <uekaram@gmail.com>
2024-01-08 18:27:09 +0900

    polls-0-20240108-end

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 # pollsフォルダ内のviews.pyをインポート
-from polls import views as views
+# from polls import views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('views/', views.index,name='index'),   # views記載の関数のpathを追記
+    path('', include('polls.urls') ),   # pollsディレクトリ内のurls.pyの参照を定義
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 # HTTPResponseクラスをインポート
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # View関数を任意に定義
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'polls/index.html')
