4d70651f8038bbe08c5981443e03b7fc0ee58a4a
sunlit-ind <uekaram@gmail.com>
2024-01-11 11:45:08 +0900

    polls-0-20240111-middle

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'polls'
+# app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'),
     # path('totalling', views.totalling, name='totalling'),
--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 # HTTPResponseクラスをインポート
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 # View関数を任意に定義
 def index(request):
+    # 変数設定
+    
+    # 出力
     return render(request, 'polls/index.html')
